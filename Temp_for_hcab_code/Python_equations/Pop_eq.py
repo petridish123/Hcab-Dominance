afe655bd768096178334aa135e503a8aa991a41e
Peter G <peter.c.gordon@gmail.com>
2026-01-30 11:04:51 -0700

    added python equations

---
+++ Temp_for_hcab_code/Python_equations/Pop_eq.py
@@ -0,0 +1,293 @@
+
+
+
+influence_1_4 : dict = {
+      "round_1": {
+        "Delta": {
+          "Delta": 0,
+          "Foxtrot": 0,
+          "November": 0,
+          "Yankee": 0,
+          "__intrinsic__": 100
+        },
+        "Foxtrot": {
+          "Delta": 0,
+          "Foxtrot": 0,
+          "November": 0,
+          "Yankee": 0,
+          "__intrinsic__": 100
+        },
+        "November": {
+          "Delta": 0,
+          "Foxtrot": 0,
+          "November": 0,
+          "Yankee": 0,
+          "__intrinsic__": 100
+        },
+        "Yankee": {
+          "Delta": 0,
+          "Foxtrot": 0,
+          "November": 0,
+          "Yankee": 0,
+          "__intrinsic__": 100
+        }
+      },
+      "round_2": {
+        "Delta": {
+          "Delta": 0,
+          "Foxtrot": 0,
+          "November": 0,
+          "Yankee": 9.750000000000002,
+          "__intrinsic__": 80
+        },
+        "Foxtrot": {
+          "Delta": 0,
+          "Foxtrot": 0,
+          "November": 0,
+          "Yankee": 0,
+          "__intrinsic__": 80
+        },
+        "November": {
+          "Delta": 26,
+          "Foxtrot": 0,
+          "November": 19,
+          "Yankee": 13,
+          "__intrinsic__": 80
+        },
+        "Yankee": {
+          "Delta": 0,
+          "Foxtrot": 26,
+          "November": 0,
+          "Yankee": 2.375,
+          "__intrinsic__": 80
+        }
+      },
+      "round_3": {
+        "Delta": {
+          "Delta": 0,
+          "Foxtrot": 0,
+          "November": 0,
+          "Yankee": 32.61815419420247,
+          "__intrinsic__": 64.00000000000001
+        },
+        "Foxtrot": {
+          "Delta": 0,
+          "Foxtrot": 0,
+          "November": 0,
+          "Yankee": 0,
+          "__intrinsic__": 64.00000000000001
+        },
+        "November": {
+          "Delta": 42.707303995193755,
+          "Foxtrot": 0,
+          "November": 43.90158606188045,
+          "Yankee": 10.617122258936622,
+          "__intrinsic__": 64.00000000000001
+        },
+        "Yankee": {
+          "Delta": 0,
+          "Foxtrot": 39.19759687593873,
+          "November": 0,
+          "Yankee": 4.513572816536498,
+          "__intrinsic__": 64.00000000000001
+        }
+      },
+      "round_4": {
+        "Delta": {
+          "Delta": 0,
+          "Foxtrot": 0,
+          "November": 0,
+          "Yankee": 29.107362120796605,
+          "__intrinsic__": 51.20000000000001
+        },
+        "Foxtrot": {
+          "Delta": 0,
+          "Foxtrot": 12.160000000000004,
+          "November": 0,
+          "Yankee": 14.002452060021781,
+          "__intrinsic__": 51.20000000000001
+        },
+        "November": {
+          "Delta": 59.99437820493715,
+          "Foxtrot": 0,
+          "November": 68.19909813473413,
+          "Yankee": 18.834325597103014,
+          "__intrinsic__": 51.20000000000001
+        },
+        "Yankee": {
+          "Delta": 0,
+          "Foxtrot": 28.046325534943584,
+          "November": 0,
+          "Yankee": 3.5431070192290046,
+          "__intrinsic__": 51.20000000000001
+        }
+      }
+      }
+
+pop_1_4 : dict = {
+      "round_1": {
+        "Delta": 100,
+        "Foxtrot": 100,
+        "November": 100,
+        "Yankee": 100
+      },
+      "round_2": {
+        "Delta": 89.75,
+        "Foxtrot": 80,
+        "November": 138,
+        "Yankee": 108.375
+      },
+      "round_3": {
+        "Delta": 96.61815419420248,
+        "Foxtrot": 64.00000000000001,
+        "November": 161.22601231601084,
+        "Yankee": 107.71116969247524
+      },
+      "round_4": {
+        "Delta": 80.30736212079661,
+        "Foxtrot": 77.3624520600218,
+        "November": 198.2278019367743,
+        "Yankee": 82.7894325541726
+      }
+    }
+
+allocations_1_4 : dict = {
+      "round_1": {
+        "Delta": {
+          "Delta": 0,
+          "Foxtrot": 0,
+          "November": 8,
+          "Yankee": 0
+        },
+        "Foxtrot": {
+          "Delta": 0,
+          "Foxtrot": 0,
+          "November": 0,
+          "Yankee": 8
+        },
+        "November": {
+          "Delta": 0,
+          "Foxtrot": 0,
+          "November": 8,
+          "Yankee": 0
+        },
+        "Yankee": {
+          "Delta": 3,
+          "Foxtrot": 0,
+          "November": 4,
+          "Yankee": 1
+        }
+      },
+      "round_2": {
+        "Delta": {
+          "Delta": 0,
+          "Foxtrot": 0,
+          "November": 8,
+          "Yankee": 0
+        },
+        "Foxtrot": {
+          "Delta": 0,
+          "Foxtrot": 0,
+          "November": 0,
+          "Yankee": 8
+        },
+        "November": {
+          "Delta": 0,
+          "Foxtrot": 0,
+          "November": 8,
+          "Yankee": 0
+        },
+        "Yankee": {
+          "Delta": 7,
+          "Foxtrot": 0,
+          "November": 0,
+          "Yankee": 1
+        }
+      },
+      "round_3": {
+        "Delta": {
+          "Delta": 0,
+          "Foxtrot": 0,
+          "November": 8,
+          "Yankee": 0
+        },
+        "Foxtrot": {
+          "Delta": 0,
+          "Foxtrot": 8,
+          "November": 0,
+          "Yankee": 0
+        },
+        "November": {
+          "Delta": 0,
+          "Foxtrot": 0,
+          "November": 8,
+          "Yankee": 0
+        },
+        "Yankee": {
+          "Delta": 1,
+          "Foxtrot": 4,
+          "November": 3,
+          "Yankee": 0
+        }
+      },
+      "round_4": {
+        "Delta": {
+          "Delta": 0,
+          "Foxtrot": 0,
+          "November": 8,
+          "Yankee": 0
+        },
+        "Foxtrot": {
+          "Delta": 0,
+          "Foxtrot": 0,
+          "November": 8,
+          "Yankee": 0
+        },
+        "November": {
+          "Delta": 0,
+          "Foxtrot": 0,
+          "November": 0,
+          "Yankee": 8
+        },
+        "Yankee": {
+          "Delta": 0,
+          "Foxtrot": 0,
+          "November": 5,
+          "Yankee": 3
+        }
+      }
+    }
+
+c_steal : float = 1.6
+c_keep : float = 0.95
+c_give : float = 1.3
+
+alpha = 0.2
+beta = 0.5
+
+number_to_name : dict = {
+    0 : "Delta",
+    1 : "Foxtrot",
+    2 : "November",
+    3 : "Yankee"
+}
+
+
+
+def c_steal_k(tau : int, t : int) -> float:
+    pass
+
+
+def long_n(tau : int, t : int) -> float:
+    pass
+
+def W_j(tau : int, t : int) -> float:
+    pass
+
+
+def V_i_j(tau : int, t : int) -> float:
+    pass
+
+def influence_i_j(tau : int, t : int) -> float: pass
+
+def popularity_i(tau : int, t : int) -> float: pass
