b207cf739507f4fb83059e4e45da4ef155cf6942
Peter G <peter.c.gordon@gmail.com>
2026-02-05 11:12:11 -0700

    Created New_W_j function

--- Temp_for_hcab_code/Python_equations/Influence_extractor.py
+++ Temp_for_hcab_code/Python_equations/Influence_extractor.py
@@ -35,5 +35,30 @@ def delta_I(tau :int, t:int,i :int, j:int) -> float:
     influence_tau_minus =  Pop_eq.influence_i_j(tau - 1,t,i,j)
 
     return influence_tau - (1-Pop_eq.alpha) *influence_tau_minus
+
+
+def new_W_j(tau:int,t:int,j:int, populations : dict =Pop_eq.pop_1_4)->float:
+    """
+    Docstring for new_W_j
     
+    :param tau: Description
+    :type tau: int
+    :param t: Description
+    :type t: int
+    :param j: Description
+    :type j: int
+    :return: Description
+    :rtype: float
+    
+    This function uses the actual popularities of tau and t rounds
+    """
+    
+    name_j : str = Pop_eq.number_to_name[j]
+    round : str = "round_" + str(tau)
+    time : str = "round_" + str(t)
+
+    return Pop_eq.beta * populations[round][name_j][name_j] + (1-Pop_eq.beta) * Pop_eq.long_n(tau,t) * populations[time][name_j][name_j]
+
+
+
 
