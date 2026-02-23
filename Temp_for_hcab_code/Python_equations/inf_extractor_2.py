7781eb7e400f14abefb50c10375998e5fb025445
Peter G <peter.c.gordon@gmail.com>
2026-02-23 10:50:27 -0700

    Update inf_extractor_2.py

--- Temp_for_hcab_code/Python_equations/inf_extractor_2.py
+++ Temp_for_hcab_code/Python_equations/inf_extractor_2.py
@@ -8,14 +8,25 @@ class GameParser:
 
 
     def __init__(self, game_obj : dict) -> None:
-        self.game_obj = game_obj
+        
+        # The following variables will hold the allocations as estimated
+        self.alloactions : dict = {} # dictionary holding round_1... delta: delta:0...etc
 
-        self.influences = game_obj["influences"]
-        self.transactions = game_obj["transactions"]
+        # Long_n vals is stored to reduce the ammount of time we hold this
+        self.long_n_vals :dict = {}
+
+        # The following variables are created from the game_obj
+        self.game_obj :dict = game_obj
+
+        self.influences :dict= game_obj["influences"]
+        self.transactions :dict= game_obj["transactions"]
+        self.popularities : dict = game_obj["popularities"]
         # alpha, beta, cGive, cKeep, cSteal
-        self.params = game_obj["gameParams"]["popularityFunctionParams"]
+        self.params :dict = game_obj["gameParams"]["popularityFunctionParams"]
 
         # Creates a dictionary int : string of player names.
+        self.player_names : dict
+        self.numplayer : int
         self.make_player_names()
 
     def make_player_names(self) -> None:
@@ -23,7 +34,8 @@ class GameParser:
         for player in self.game_obj["players"]:
             player_list.append(player["gameName"])
         player_list.sort()
-        self.player_names = {}
+        self.numplayer = len(player_list)
+        self.player_names :dict[int:str] = {}
         for i in range(len(player_list)):
             self.player_names[i] = player_list[i]
         
@@ -50,3 +62,55 @@ class GameParser:
         influence_tau = Pop_eq.influence_i_j(tau,t,i,j)
         influence_tau_minus =  Pop_eq.influence_i_j(tau - 1,t,i,j)
         return influence_tau - (1-Pop_eq.alpha) * influence_tau_minus
+
+    def x_i_j_positive(self, tau : int, t :int , i: int, j:int) -> float:
+        """
+        This returns the positive allocations from player j to player i
+        """
+        name_i :str = self.player_names[i]
+        name_j :str = self.player_names[j]
+        round_name : str = "round_" + str(tau)
+
+        allocation : float = self.alloactions[round_name][name_j][name_i] 
+        if  allocation > 0:
+            return allocation
+        else:
+            return 0
+        
+    def x_i_j_negative(self, tau: int, t:int, i: int,j :int) -> float:
+        """
+        This returns negative allocations from player j to player i, 
+        0 if they are not negative
+        """
+        name_i :str = self.player_names[i]
+        name_j :str = self.player_names[j]
+        round_name : str = "round_" + str(tau)
+
+        allocation : float = self.alloactions[round_name][name_j][name_i] 
+        if allocation < 0:
+            return allocation
+        else:
+            return 0
+    
+    def c_steal_k(self, tau :int ,t:int,k:int) ->float:
+        """
+        This should calcualte the c_steal, but for now this will just do the parameter
+        """
+        return self.params["cSteal"]
+
+    def long_n(self, tau:int,t:int) -> float:
+        
+        if (tau,t) in self.long_n_vals:
+            return self.long_n_vals[(tau,t)]
+        
+        sum_tau_tau : int = 0
+        sum_t_t : int = 0
+        this_round : str = "round_" + str(tau)
+        for i in range(self.numplayer):
+            sum_tau_tau += self.popularities[this_round][self.player_names[i]]
+            # also need to get sum t t
+        
+        if not sum_t_t: sum_t_t = 1
+
+        self.long_n_vals[(tau,t)] = sum_tau_tau / sum_t_t
+        return self.long_n_vals[(tau,t)]
