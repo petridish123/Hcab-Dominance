585f2b951a25cd5cd8868fafc637a188cea6e144
Peter G <peter.c.gordon@gmail.com>
2026-03-04 10:49:58 -0700

    Update game_creator.py
    Started working on Unit tests for the game creator. Finished making the game parser. May not be as efficient as I want since this is redoing a lot of searching the dictionary. I might want to make a function that combines all of them.

--- Temp_for_hcab_code/Python_equations/game_creator.py
+++ Temp_for_hcab_code/Python_equations/game_creator.py
@@ -34,19 +34,52 @@ class game_obj:
     def game_obj(self) -> dict:
         return self.game
     
-    def get_player_names(self,game:dict) -> dict:
+    def get_player_names(self,game:dict) -> list:
         """
         Goes into the first item checks if names in item
         needs to sort by alphabetical though...
         then returns 0:name1
         ...
         """
+        return self.find_in_dict(game, "names").sort()
 
     def get_params(self, game:dict) -> dict:
-        pass
+        return self.find_in_dict(game, "param")
 
     def get_influences(self, game:dict)->dict:
-        pass
+        return self.find_in_dict(game, "influence")
 
     def get_popularities(self,game:dict)->dict:
-        pass
+        return self.find_in_dict(game, "popular") # Because it could be popularities or popularity
+
+    def find_in_dict(self, dictionary : dict, name : str)-> None|dict|list:
+        """
+        Will return a list or a dictionary or None
+        
+        recursively searches the dictionary unitl it finds the item, or searches the entire thing and doesn't find it
+        """
+
+        for key, item in dictionary.items():
+            if name.lower() in str(key).lower():
+                return item
+        
+        for key, item in dictionary.items():
+            if isinstance(item, dictionary):
+                possibility = self.find_in_dict(item, name)
+                if possibility:
+                    return possibility
+        
+        return None
+    
+
+def main() -> None:
+    """
+    Unit tests
+    """
+    
+
+
+
+
+if __name__ == "__main__":
+    main()
